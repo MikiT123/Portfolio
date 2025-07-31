f427d9a898cf0ae29d6772612e83bae1eae75f2a
Michaela Tang <161909150+MikiT123@users.noreply.github.com>
2025-07-31 16:00:23 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+function showAcademics() {
+  document.getElementById("homePage").classList.add("hidden");
+  document.getElementById("academicsPage").classList.remove("hidden");
+}
+
+function returnHome() {
+  document.getElementById("academicsPage").classList.add("hidden");
+  document.getElementById("homePage").classList.remove("hidden");
+}
